fe347aae88ea6abf6c8d62c1f19247f23c4166f1
Markus Wesslén <markus.wesslen@gmail.com>
2025-10-29 21:31:00 +0100

    Add bookingCreated to revenue table
    Used for economic forecasting.

--- sql/calculated/1_revenue.sql
+++ sql/calculated/1_revenue.sql
@@ -1,31 +1,37 @@
-SELECT bookingId,
-    bookingName,
-    bookingPricePlan,
-    bookingAccountKind,
-    bookingType,
-    bookingInvoiceDate,
-    bookingOperationalYear,
-    bookingFiscalYear,
-    `name`,
-    account,
-    totalPrice as amount,
-    'equipment' as revenueType
-FROM `rn-admin-391316.raw_backstage2.equipmentUsage`
-WHERE bookingFixedPrice is null
-    and bookingStatus = 'Klar'
-UNION ALL
-SELECT bookingId,
-    bookingName,
-    bookingPricePlan,
-    bookingAccountKind,
-    bookingType,
-    bookingInvoiceDate,
-    bookingOperationalYear,
-    bookingFiscalYear,
-    userName as `name`,
-    account,
-    totalPrice as amount,
-    'timeReport' as revenueType
-FROM `rn-admin-391316.raw_backstage2.timeReport`
-WHERE bookingFixedPrice is null
-    and bookingStatus = 'Klar'
+WITH revenue AS (
+    SELECT bookingId,
+        bookingName,
+        bookingPricePlan,
+        bookingAccountKind,
+        bookingType,
+        bookingInvoiceDate,
+        bookingOperationalYear,
+        bookingFiscalYear,
+        `name`,
+        account,
+        totalPrice as amount,
+        'equipment' as revenueType
+    FROM `rn-admin-391316.raw_backstage2.equipmentUsage` AS equipmentUsage
+    WHERE bookingFixedPrice is null
+        and bookingStatus = 'Klar'
+    UNION ALL
+    SELECT bookingId,
+        bookingName,
+        bookingPricePlan,
+        bookingAccountKind,
+        bookingType,
+        bookingInvoiceDate,
+        bookingOperationalYear,
+        bookingFiscalYear,
+        userName as `name`,
+        account,
+        totalPrice as amount,
+        'timeReport' as revenueType
+    FROM `rn-admin-391316.raw_backstage2.timeReport`
+    WHERE bookingFixedPrice is null
+        and bookingStatus = 'Klar'
+)
+SELECT revenue.*,
+    booking.created as bookingCreated
+FROM revenue
+    LEFT JOIN `rn-admin-391316.raw_backstage2.booking` AS booking ON revenue.bookingId = booking.id

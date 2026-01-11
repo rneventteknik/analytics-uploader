7448cd92d03846aaefe68bf881afebdfd79f977e
Alexander <aeakoski@yahoo.se>
2026-01-11 15:34:50 +0100

    Add accountKind null check to revenueIncludingProjected view

--- sql/calculated/6_revenueIncludingProjected.sql
+++ sql/calculated/6_revenueIncludingProjected.sql
@@ -49,6 +49,7 @@ WITH revenue AS (
     FROM `rn-admin-391316.raw_backstage2.booking`
     WHERE fixedPrice is null
         and status != 'Inställd'
+        and accountKind is not null
 )
 SELECT revenue.*,
     booking.created as bookingCreated,

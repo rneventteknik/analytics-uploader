9ea42e54b46305725eed3a5710d988db8f6b1c95
Alexander <aeakoski@yahoo.se>
2026-01-11 15:29:01 +0100

    Add time estimates to revenueIncludingProjected view

--- sql/calculated/6_revenueIncludingProjected.sql
+++ sql/calculated/6_revenueIncludingProjected.sql
@@ -30,6 +30,25 @@ WITH revenue AS (
     FROM `rn-admin-391316.raw_backstage2.timeReport`
     WHERE bookingFixedPrice is null
         and bookingStatus != 'Inställd'
+    UNION ALL
+    SELECT id as bookingId,
+        name as bookingName,
+        pricePlan as bookingPricePlan,
+        accountKind as bookingAccountKind,
+        bookingType,
+        invoiceDate as bookingInvoiceDate,
+        operationalYear as bookingOperationalYear,
+        fiscalYear as bookingFiscalYear,
+        "Tidsestimat" as `name`,
+        case
+            when accountKind = "Normal" then 3620
+            when accountKind = "Intern" then 7900
+        end as account,
+        totalTimeEstimatesPrice as amount,
+        'timeEstimate' as revenueType
+    FROM `rn-admin-391316.raw_backstage2.booking`
+    WHERE fixedPrice is null
+        and status != 'Inställd'
 )
 SELECT revenue.*,
     booking.created as bookingCreated,

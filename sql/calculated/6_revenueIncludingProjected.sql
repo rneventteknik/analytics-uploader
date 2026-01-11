2eac079f3ae6f60a2d260b34ab5b359abc75e6bf
Alexander <aeakoski@yahoo.se>
2026-01-11 15:05:42 +0100

    Add revenueIncludingProjected view with projected revenue

---
+++ sql/calculated/6_revenueIncludingProjected.sql
@@ -0,0 +1,38 @@
+WITH revenue AS (
+    SELECT bookingId,
+        bookingName,
+        bookingPricePlan,
+        bookingAccountKind,
+        bookingType,
+        bookingInvoiceDate,
+        bookingOperationalYear,
+        bookingFiscalYear,
+        `name`,
+        account,
+        totalPrice as amount,
+        'equipment' as revenueType
+    FROM `rn-admin-391316.raw_backstage2.equipmentUsage` AS equipmentUsage
+    WHERE bookingFixedPrice is null
+        and bookingStatus != 'Inställd'
+    UNION ALL
+    SELECT bookingId,
+        bookingName,
+        bookingPricePlan,
+        bookingAccountKind,
+        bookingType,
+        bookingInvoiceDate,
+        bookingOperationalYear,
+        bookingFiscalYear,
+        userName as `name`,
+        account,
+        totalPrice as amount,
+        'timeReport' as revenueType
+    FROM `rn-admin-391316.raw_backstage2.timeReport`
+    WHERE bookingFixedPrice is null
+        and bookingStatus != 'Inställd'
+)
+SELECT revenue.*,
+    booking.created as bookingCreated,
+    booking.usageStartDatetime as bookingUsageStartDatetime
+FROM revenue
+    LEFT JOIN `rn-admin-391316.raw_backstage2.booking` AS booking ON revenue.bookingId = booking.id

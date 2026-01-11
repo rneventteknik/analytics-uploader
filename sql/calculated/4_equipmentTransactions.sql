a7fdabd0067062e04af420a3699ddb57d71d4299
Gabriel Ingemarsson <gabriel@ingemarsson.nu>
2026-01-11 22:48:48 +0100

    Update equipment transactions with stage1 data (#4)

--- sql/calculated/4_equipmentTransactions.sql
+++ sql/calculated/4_equipmentTransactions.sql
@@ -2,7 +2,8 @@
     SELECT `equipmentId`,
         `equipmentName`,
         `totalPrice` AS amount,
-        cast(bookingInvoiceDate AS date) AS `transactionDate`
+        cast(bookingInvoiceDate AS date) AS `transactionDate`,
+        bookingName as comment
     FROM `rn-admin-391316.raw_backstage2.equipmentUsage`
     WHERE bookingInvoiceDate IS NOT NULL
         AND equipmentId IS NOT NULL
@@ -13,7 +14,18 @@ UNION ALL
     SELECT `equipmentId`,
         `equipmentName`,
         -(amount),
-        `investmentDate` AS `transactionDate`
+        `investmentDate` AS `transactionDate`,
+        comment
     FROM `rn-admin-391316.raw_spreadsheet.equipment_investments`
+)
+UNION ALL
+(
+    SELECT `equipmentId`,
+        `equipmentName`,
+        (amount),
+        `investmentDate` AS `transactionDate`,
+        comment
+    FROM `rn-admin-391316.raw_spreadsheet.equipment_stage1_income`
+    WHERE equipmentId IS NOT NULL
 )
 ORDER BY transactionDate
